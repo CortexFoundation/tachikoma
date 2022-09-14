dfc305c9f176158c8ec62ce12d94ec2d20241b8e
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-14 12:51:25 -0400

    Update tachikoma_json_runtime.cc

--- src/runtime/contrib/tachikoma/tachikoma_json_runtime.cc
+++ src/runtime/contrib/tachikoma/tachikoma_json_runtime.cc
@@ -491,9 +491,9 @@ runtime::Module TachikomaJSONRuntimeCreate(String symbol_name, String graph_json
 }
 
 void TachikomaExportModule(runtime::Module mod) {
-  tvm::runtime::PackedFunc exportEntries = mod.GetFunction("export_data_entries");
-  exportEntries();
-  std::cerr << "Exporting module successful." << std::endl;
+  tvm::runtime::PackedFunc exportEntries = mod.GetFunction("export_data_entries", false);
+  // exportEntries();
+  std::cerr << "Exporting module successful." << exportEntries == nullptr << std::endl;
 }
 
 TVM_REGISTER_GLOBAL("runtime.TachikomaJSONRuntimeCreate").set_body_typed(TachikomaJSONRuntimeCreate);
