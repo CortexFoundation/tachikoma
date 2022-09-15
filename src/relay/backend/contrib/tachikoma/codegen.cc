ae7bf54e3d153ca64c17ad4a5ca2e2d1f4199969
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-14 23:02:27 -0400

    Update codegen.cc

--- src/relay/backend/contrib/tachikoma/codegen.cc
+++ src/relay/backend/contrib/tachikoma/codegen.cc
@@ -468,8 +468,7 @@ class TachikomaJSONSerializer : public backend::contrib::JSONSerializer {
       } else if (name.find("tachikoma.conv1d") != std::string::npos) {
         call = GetRootCall(fn->body.as<CallNode>(), 10, "nn.conv1d");
         ICHECK(call->op.as<OpNode>()) << "Not op node";
-      } else */ 
-      if (name.find("tachikoma.conv2d") != std::string::npos) {
+      } else if (name.find("tachikoma.conv2d") != std::string::npos) {
         call = GetRootCall(fn->body.as<CallNode>(), 10, "nn.conv2d");
         ICHECK(call->op.as<OpNode>()) << "Not op node";
       } else if (name.find("tachikoma.conv3d") != std::string::npos) {
