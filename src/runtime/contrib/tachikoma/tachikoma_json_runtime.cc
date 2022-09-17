def185c289f32f4c894655bca6f0a2ce73a2dba1
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-17 17:48:19 -0400

    Update tachikoma_json_runtime.cc

--- src/runtime/contrib/tachikoma/tachikoma_json_runtime.cc
+++ src/runtime/contrib/tachikoma/tachikoma_json_runtime.cc
@@ -103,7 +103,7 @@ class TachikomaJSONRuntime : public JSONRuntimeBase {
     std::cerr << "[writing to " << path_name << " ...]" << std::endl;
     std::cerr << (void*) this << " " << this->symbol_name_ << std::endl;
     std::cerr << d.size() << " vectors in total." << std::endl;
-    boost::filesystem::create_directories::create_directories(path_name);
+    boost::filesystem::create_directories(path_name);
     for (size_t vector_id = 0; vector_id < d.size(); vector_id++) {
           const DLTensor* tensor = d[vector_id];
           std::string data;
