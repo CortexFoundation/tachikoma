57cfff8551d9c38e336ddbd3658a2b71f8f83f8c
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-16 20:56:13 -0400

    Update tachikoma.py

--- python/tvm/relay/op/contrib/tachikoma.py
+++ python/tvm/relay/op/contrib/tachikoma.py
@@ -233,6 +233,6 @@ def partition_for_tachikoma(mod, params=None):
             transform.PartitionGraph(),
         ]
     )
-    with tvm.transform.PassContext(opt_level=1):
+    with tvm.transform.PassContext(opt_level=2):
         mod = seq(mod)
     return mod
