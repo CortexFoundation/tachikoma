85cb66652dab26a1fc398f152418268bfc1a2758
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-18 20:27:08 -0400

    Revert "move partition_for_tachikoma"
    This reverts commit 34e34237a1f68cef642415fe15b017fcddbb07f2.

--- python/tvm/relay/op/contrib/tachikoma.py
+++ python/tvm/relay/op/contrib/tachikoma.py
@@ -34,9 +34,6 @@ import logging
 
 import tvm.ir
 from tvm import relay
-from tvm.relay import transform
-from tvm.relay.build_module import bind_params_by_name
-from tvm.relay.testing.temp_op_attr import TempOpAttr
 
 from ... import _ffi_api
 from ...dataflow_pattern import wildcard, is_op
@@ -202,73 +199,6 @@ def pattern_table():
                 tachikoma_patterns.append(make_tachikoma_pattern(conv_name, with_bias, elt))
             tachikoma_patterns.append(make_tachikoma_pattern("nn.dense", with_bias, elt))
     return tachikoma_patterns
-
-
-def partition_for_tachikoma(mod, params=None, alter_layout=True):
-    """Partition the graph greedily offloading supported operators to Tachikoma.
-    Parameters
-    ----------
-    mod : Module
-        The module to run passes on.
-    params : Optional[Dict[str, NDArray]]
-        Constant input parameters.
-    Returns
-    -------
-    mod : Module
-        Annotated and partitioned module.
-    """
-    if params:
-        mod["main"] = bind_params_by_name(mod["main"], params)
-
-    with TempOpAttr("nn.conv2d", "FTVMLegalize", tachikoma.legalize_group_conv):
-        with TempOpAttr("nn.conv2d_transpose", "FTVMLegalize", tachikoma.legalize_group_conv):
-            seq = tvm.transform.Sequential(
-                [
-                    transform.CanonicalizeOps(),
-                    transform.InferType(),
-                    transform.SimplifyInference(),
-                    transform.FoldConstant(),
-                    transform.FoldScaleAxis(),
-                    # fold consecutive add ops to simplify pattern `conv2d-bias_add-bn-relu`
-                    transform.SimplifyExpr(),
-                    transform.FoldConstant(),
-                    # alter group conv /conv_transpose layout to `GOIHW` / `GIOHW`
-                    transform.Legalize(),
-                    transform.FoldConstant(),
-                ]
-            )
-            with tvm.transform.PassContext(opt_level=3):
-                mod = seq(mod)
-    if alter_layout:
-        with TempOpAttr("nn.conv1d", "FTVMAlterOpLayout", tachikoma.alter_conv):
-            with TempOpAttr("nn.conv2d", "FTVMAlterOpLayout", tachikoma.alter_conv):
-                with TempOpAttr("nn.conv3d", "FTVMAlterOpLayout", tachikoma.alter_conv):
-                    with TempOpAttr(
-                        "nn.conv2d_transpose", "FTVMAlterOpLayout", tachikoma.alter_conv_transpose
-                    ):
-                        with TempOpAttr(
-                            "nn.conv3d_transpose", "FTVMAlterOpLayout", tachikoma.alter_conv_transpose
-                        ):
-                            alter_layout_seq = tvm.transform.Sequential(
-                                [
-                                    transform.AlterOpLayout(),
-                                    transform.FoldConstant(),
-                                ]
-                            )
-                            with tvm.transform.PassContext(opt_level=3):
-                                mod = alter_layout_seq(mod)
-
-    byoc_seq = tvm.transform.Sequential(
-        [
-            transform.MergeComposite(tachikoma.pattern_table()),
-            transform.AnnotateTarget("tachikoma"),
-            transform.MergeCompilerRegions(),
-            transform.PartitionGraph(),
-        ]
-    )
-    with tvm.transform.PassContext(opt_level=3):
-        mod = byoc_seq(mod)
-    return mod
 
 
 def get_optimal_layout_for_conv(
