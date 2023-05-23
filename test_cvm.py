fa81f487c67f2b85f275cd09fab75a4b11c3951a
remloveh <zhouhang@cortexlabs.ai>
2023-05-23 16:36:11 +0800

    update test_cvm and sym_cvm

--- test_cvm.py
+++ test_cvm.py
@@ -147,143 +147,11 @@ qt_tr = dt_tr.checkpoint_transform(
 # qt_tr.log()
 qt_tr.print(short=True)
 
+from tvm.mrt.sym_cvm import to_cvm
 import cvm
 
-op_dict = {
-    'sum': 'sum',
-    'squeeze': 'squeeze',
-    'nn.conv2d': 'conv2d',
-    'nn.dense': 'dense',
-    'nn.max_pool2d': 'max_pool2d',
-    'mrt.rs_pclip': 'cvm_right_shift',
-    'right_shift': 'cvm_right_shift',
-    'multiply': 'broadcast_mul',
-    'mrt.pclip': 'cvm_clip',
-    'add': 'elemwise_add',
-    'reshape': 'reshape',
-    'nn.relu': 'relu'
-}
-
-
-def get_node(nodes: list, node_name: str) -> list:
-    for index in range(len(nodes)):
-        if nodes[index]['name'] == node_name:
-            return [index, 0, 0]
-
-
-def to_cvm(symbol: Symbol) -> dict:
-    cvm_data = {}
-    cvm_nodes = []
-    cvm_arg_nodes = []
-    cvm_node_row_ptr = []
-    cvm_attrs = {
-        "op_attrs": ["list_str", []],
-        "dltype": ["list_str", []],
-        "precision": ["list_int", []],
-        "storage_id": ["list_int", []],
-        "shape": ["list_shape", []]
-    }
-    sym_list = sym2list(symbol)
-    # transform symbol args
-    for i in range(len(sym_list)):
-        if sym_list[i].op_name != 'var' and sym_list[i].args[0].op_name == 'cast':
-            sym_list[i].args[0] = sym_list[i].args[0].args[0]
-        if sym_list[i].op_name == 'mrt.rs_pclip':
-            shift_bit = int(qt_pa[sym_list[i].args[1].name].asnumpy())
-            if int(qt_pa[sym_list[i].args[1].name].asnumpy()) > 32:
-                shift_bit = 32
-            sym_list[i].attrs['shift_bit'] = shift_bit
-            del(cvm_params[sym_list[i].args[1].name])
-            del(sym_list[i].args[1])
-        if sym_list[i].op_name == 'right_shift':
-            shift_bit = int(qt_pa[sym_list[i].args[1].name].asnumpy())
-            sym_list[i].attrs['shift_bit'] = shift_bit
-            sym_list[i].attrs['precision'] = sym_list[i].extra_attrs['precision']
-            del(cvm_params[sym_list[i].args[1].name])
-            del(sym_list[i].args[1])
-        if sym_list[i].op_name == 'nn.bias_add':
-            sym_list[i].op_name = sym_list[i].args[0].args[0].op_name
-            sym_list[i].attrs = sym_list[i].args[0].args[0].attrs
-            sym_list[i].args.append(sym_list[i].args[1])
-            sym_list[i].args[1] = sym_list[i].args[0].args[0].args[1]
-            sym_list[i].args[0] = sym_list[i].args[0].args[0].args[0]
-        if sym_list[i].op_name == 'multiply':
-            sym_list[i - 1].extra_attrs['precision'] = 16
-    # symbol topo sort
-    index = 0
-    for sym in sym2list(symbol):
-        if sym.op_name == 'nn.conv2d':
-            sym.attrs['layout'] = sym.attrs['data_layout']
-            del(sym.attrs['data_layout'])
-            sym.attrs['out_dtype'] = 'int32'
-            del(sym.attrs['out_layout'])
-            del(sym.attrs['padding'][2:])
-        elif sym.op_name == 'nn.max_pool2d':
-            del(sym.attrs['out_layout'])
-            del(sym.attrs['dilation'])
-            del(sym.attrs['padding'][2:])
-        elif sym.op_name == 'reshape':
-            del(sym.attrs['allowzero'])
-            sym.attrs['shape'] = sym.attrs['newshape']
-            del(sym.attrs['newshape'])
-        elif sym.op_name == 'nn.dense':
-            del(sym.attrs['out_dtype'])
-            sym.attrs['units'] = sym.extra_attrs['shape'][1]
-        for key in sym.attrs:
-            sym.attrs[key] = str(sym.attrs[key])
-        cvm_attrs["op_attrs"][1].append(json.dumps(sym.attrs))
-        cvm_attrs["dltype"][1].append("int32")
-        try:
-            precision = sym.extra_attrs['precision']
-        except KeyError:
-            precision = -1
-        cvm_attrs['precision'][1].append(precision)
-        shape = sym.extra_attrs['shape']
-        shape = list(shape)
-        if shape == []:
-            shape = [1]
-        cvm_attrs['shape'][1].append(shape)
-        cvm_attrs['storage_id'][1].append(index)
-        op = sym.op_name
-        inputs = []
-        args = sym.args
-        if op == "var":
-            op = "null"
-            cvm_arg_nodes.append(index)
-            cvm_nodes.append({
-                "op": op,
-                "name": sym.name,
-                "inputs": inputs
-                })
-        else:
-            for arg in args:
-                inputs.append(get_node(cvm_nodes, arg.name))
-            attrs = {}
-            attrs["flatten_data"] = "0"
-            attrs["func_name"] = op_dict[op]
-            attrs["num_inputs"] = str(len(args))
-            attrs["num_outputs"] = "1"
-            op = "cvm_op"
-            cvm_nodes.append({
-                "op": op,
-                "name": sym.name,
-                "attrs": attrs,
-                "inputs": inputs
-                })
-        cvm_node_row_ptr.append(index)
-        index = index + 1
-    cvm_nodes[0]['name'] = 'data'
-    cvm_node_row_ptr.append(index)
-    cvm_data["nodes"] = cvm_nodes
-    cvm_data["arg_nodes"] = cvm_arg_nodes
-    cvm_data["node_row_ptr"] = cvm_node_row_ptr
-    cvm_data["heads"] = [[index-1, 0, 0]]
-    cvm_data["attrs"] = cvm_attrs
-    return cvm_data
-
-
 # params
-print('Transform TVM params into CVM')
+print("Transform TVM params into CVM")
 cvm_params = {}
 qt_pa = qt_tr.params
 for key in qt_pa:
@@ -292,36 +160,36 @@ for key in qt_pa:
     else:
         np_data = qt_pa[key].asnumpy().astype("int32")
     cvm_params[key] = cvm.nd.array(np_data)
-print('Transform TVM symbol into CVM finished')
+print("Transform TVM symbol into CVM finished")
 
 # symbol
-print('Transform TVM symbol into CVM')
+print("Transform TVM symbol into CVM")
 qt_sy = qt_tr.symbol
-qt_dict = to_cvm(qt_sy)
-print('Transform TVM symbol into CVM finished')
+qt_dict = to_cvm(qt_sy, qt_pa, cvm_params)
+print("Transform TVM symbol into CVM finished")
 
-print('CVM Json&Params dump')
+print("CVM Json&Params dump")
 with open("data/resnet18_v1/symbol", "w") as f:
-    f.write(json.dumps(qt_dict, indent=2, ensure_ascii=False))
-param_bytes = cvm.nd.save_param_dict(cvm_params)
+    f.write(json.dumps(qt_dict["symbol"], indent=2, ensure_ascii=False))
+param_bytes = cvm.nd.save_param_dict(qt_dict["params"])
 with open("data/resnet18_v1/params", "wb") as f:
     f.write(param_bytes)
 
-# from tvm.mrt.zkml import circom, transformer
+from tvm.mrt.zkml import circom, transformer
 
-# print(">>> Generating circom code ...")
-# symbol, params = qt_tr.symbol, qt_tr.params
-# symbol = transformer.shape_adapter(symbol)
-# out = transformer.model2circom(symbol, params)
-# code = circom.generate(out)
-# input_json = transformer.input_json(symbol, params)
+print(">>> Generating circom code ...")
+symbol, params = qt_tr.symbol, qt_tr.params
+symbol = transformer.shape_adapter(symbol)
+out = transformer.model2circom(symbol, params)
+code = circom.generate(out)
+input_json = transformer.input_json(symbol, params)
 
-# print(">>> Generated, dump to {} ...".format(args.output))
-# #  print(code)
-# with open(args.output + ".circom", "w") as f:
-#     f.write(code)
-# with open(args.output + ".json", "w") as f:
-#     f.write(json.dumps(input_json, indent=2))
+print(">>> Generated, dump to {} ...".format(args.output))
+#  print(code)
+with open(args.output + ".circom", "w") as f:
+    f.write(code)
+with open(args.output + ".json", "w") as f:
+    f.write(json.dumps(input_json, indent=2))
 
 
 sys.exit(-1)
